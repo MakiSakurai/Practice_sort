73a962a89f96cc99bff84f5c8b559a36fed4927f
MakiSakurai <s20c1047sb@s.chibakoudai.jp>
2021-05-05 20:29:09 +0900

    Update: コード更新

--- bubble_sort/bubble_matplotlib.py
+++ bubble_sort/bubble_matplotlib.py
@@ -3,9 +3,19 @@
 
 import math
 import numpy as np
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
 
-rand = np.random.randint(4, 10, 10)
-
-
-
+rand = np.random.randint(1, 100, 100)
+x = np.arange(len(rand))
+n = len(rand)
+for i in range(n - 1):
+    for j in range ((n - i) - 1):
+        if rand[j] > rand[j + 1]:
+            rand[j], rand[j + 1] = rand[j + 1], rand[j]
+            line = plt.bar(x, rand, color='blue')
+            plt.pause(0.01)
+            line.remove()
+            print(rand)
+plt.bar(x, rand, color='blue')
+plt.show()
+print(rand)
